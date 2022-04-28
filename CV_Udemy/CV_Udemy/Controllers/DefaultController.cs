72e719cd1881fde28c73457a350cf90c9ffde161
Cemil Kağan Gürkan <53119042+cemil17@users.noreply.github.com>
2022-04-28 13:07:06 +0300

    MVC5 CV sitesi
    Murat Yücedağ'ın MVC5 video serisi destekli bir proje

---
+++ CV_Udemy/CV_Udemy/Controllers/DefaultController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CV_Udemy.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace CV_Udemy.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        CV_SiteEntities entities = new CV_SiteEntities();
+        public ActionResult Index()
+        {
+            var list = entities.TBL_About.ToList();
+            return View(list);
+        }
+        public PartialViewResult SocialMedia()
+        {
+            var media = entities.TBL_SocialMedia.Where(x => x.State == true).ToList();
+            return PartialView(media);
+        }
+        public PartialViewResult Experience()
+        {
+            var experience = entities.TBL_Experience.OrderByDescending(x => x.ID).ToList();
+            return PartialView(experience);
+        }
+
+        public PartialViewResult Education()
+        {
+            var edu = entities.TBL_Education.ToList();
+            return PartialView(edu);
+        }
+        public PartialViewResult Talents()
+        {
+            var talent = entities.TBL_Skills.ToList();
+            return PartialView(talent);
+        }
+        public PartialViewResult Hobby(int number = 1)
+        {
+            var hobby = entities.TBL_Hobby.OrderByDescending(x => x.ID).ToList().ToPagedList(number, 20);
+            var max1 = entities.TBL_Hobby.Count().ToString();
+            ViewBag.d = max1;
+            return PartialView(hobby);
+
+        }
+        public PartialViewResult Certicate()
+        {
+            var certificate = entities.TBL_Certificate.OrderByDescending(x => x.ID).ToList();
+            return PartialView(certificate);
+        }
+        [HttpGet]
+        public PartialViewResult Contact()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Contact(TBL_Contact c)
+        {
+            if (!ModelState.IsValid)
+            {
+                return PartialView("Contact");
+            }
+            c.DateTime = DateTime.Parse(DateTime.Now.ToShortDateString());
+            entities.TBL_Contact.Add(c);
+            entities.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ CV_Udemy/CV_Udemy/Controllers/HobbyController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CV_Udemy.Repositories;
+using CV_Udemy.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace CV_Udemy.Controllers
+{
+    public class HobbyController : Controller
+    {
+        // GET: Hobby
+        GenericRepository<TBL_Hobby> repo = new GenericRepository<TBL_Hobby>();
+        CV_SiteEntities entities = new CV_SiteEntities();
+
+        public ActionResult Index(int number = 1)
+        {
+            var hobby = repo.List().ToPagedList(number, 10);
+            var max1 = entities.TBL_Hobby.Count().ToString();
+            ViewBag.a = max1;
+            return View(hobby);
+        }
+        [HttpGet]
+        public ActionResult NewHobby()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHobby(TBL_Hobby h)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("NewHobby");
+            }
+            repo.TAdd(h);
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveHobby(int id)
+        {
+            var hobby = repo.Find(c => c.ID == id);
+            repo.TDelete(hobby);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateHobby(int id)
+        {
+            var hobby = repo.Find(x => x.ID == id);
+            return View(hobby);
+        }
+        [HttpPost]
+        public ActionResult UpdateHobby(TBL_Hobby h)
+        {
+            var hobby = repo.Find(c => c.ID == h.ID);
+            hobby.Description = h.Description;
+            hobby.Description2 = h.Description2;
+            repo.TUpdate(hobby);
+            return RedirectToAction("Index");
+        }
+    }
+}
